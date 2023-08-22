e0d8d65588ba57dc1a1f2e7f49f3214e711c1b3b
psy3720 <qkrtmddyd12@naver.com>
2023-08-23 00:31:40 +0900

    feat: message change

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello deploy";
+        return "hello deploy test";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
